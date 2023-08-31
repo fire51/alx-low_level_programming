c8c539b514091cfd66115f9a5791da2a633f9693
fire51 <firehiwotbelay01@gmail.com>
2023-08-31 13:19:46 -0700

    bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,17 +5,20 @@
 *@b is pointing to a string of 0 and 1 chars
 *return: unsigned int
 **/
-unsigned int binary_to_uint(const char *b);
+unsigned int binary_to_uint(const char *b)
 {
-Unsigned int value = 0;
-if (!b)
-return(0);
-while(*b)
-{
-if(*b !='0' && *b ! = '1')
-value = value *2 + (*b++ -'0');
+	int i;
+	unsigned int value = 0;
 
-}
-return (value);
-}
+	if (!b)
+		return (0);
 
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		value = 2 * value + (b[i] - '0');
+	}
+
+	return (valu);
+}
